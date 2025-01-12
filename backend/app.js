637b47703e61b8ad54ba314acf6461818ecc96d9
Hardik Rajput <hardikrajput9@gmail.com>
2025-01-13 00:00:25 +0530

    initial changes Commit

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+
 export const app = express();
 
 app.use(express.json({ limit: "50mb" }));
@@ -7,5 +8,11 @@ app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
 import { userRouter } from "./routes/User.js";
-
 app.use("/api/v1", userRouter);
+
+// import path from "path";
+// app.use(express.static(path.resolve("./frontend/build")));
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve("./frontend/build/index.html"));
+// });
